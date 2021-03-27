56fb0829576347ebc439cdb9ec9d4cebdb22bc0d
dimbee40 <diminutive.bee@gmail.com>
2021-03-27 22:19:50 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,7 +44,9 @@ function weather(response) {
   cityWeather.innerHTML = `${temperature}`;
   let cityDescription = document.querySelector("#descriptor");
   console.log(response.data.weather[0].description);
-  cityDescription.innerHTML = response.data.weather[0].description;
+  cityDescription.innerHTML = text.toUppercase(
+    response.data.weather[0].description
+  );
 }
 
 let form = document.querySelector("#search");
