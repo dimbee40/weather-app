67b4edd0e93d7a1c75b648d02afc20bb15d20e34
dimbee40 <diminutive.bee@gmail.com>
2021-03-28 21:15:40 +1100

    added geolocator default display weather

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function weather(response) {
   let cityWeather = document.querySelector("#temperature");
   console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
-  cityWeather.innerHTML = `${temperature}`;
+  cityWeather.innerHTML = `${temperature} °C`;
   let cityDescription = document.querySelector("#descriptor");
   console.log(response.data.weather[0].description);
   cityDescription.innerHTML = response.data.weather[0].description;
@@ -63,30 +63,29 @@ function weather(response) {
     "src",
     `http://openweathermap.org/img/wn/${icon}@2x.png`
   );
+
+  function funits(event) {
+    event.preventDefault();
+    let unitsTemp = document.querySelector("#temperature");
+    console.log(temperature.value);
+    let calculate = Math.round(temperature * (9 / 5) + 32);
+    unitsTemp.innerHTML = `${calculate}°F`;
+    let apparentFaren = Math.round(feelTemp * (9 / 5) + 32);
+    apparentTemp.innerHTML = `Apparent temperature: Feels like ${apparentFaren}°F`;
+  }
+  function cunits(event) {
+    event.preventDefault();
+    let unitsTemp = document.querySelector("#temperature");
+    unitsTemp.innerHTML = 19;
+  }
+
+  let clickUnitF = document.querySelector("#unit-id");
+  clickUnitF.addEventListener("click", funits);
 }
 
 //C to F converstion *****
 //F to C-->  (°F − 32) × 5/9 = °C
 // C to F --> (°C × 9/5) + 32 = °F
-
-function funits(event) {
-  event.preventDefault();
-  let unitsTemp = document.querySelector("#temperature");
-  console.log(temperature.value);
-  let calculate = temperature.value(9 / 5) + 32;
-  unitsTemp.innerHTML = `${calculate}`;
-}
-function cunits(event) {
-  event.preventDefault();
-  let unitsTemp = document.querySelector("#temperature");
-  unitsTemp.innerHTML = 19;
-}
-
-let clickUnitF = document.querySelector("#faren-id");
-clickUnitF.addEventListener("click", funits);
-
-let clickUnitC = document.querySelector("#celsius-id");
-clickUnitC.addEventListener("click", cunits);
 
 let form = document.querySelector("#search");
 form.addEventListener("submit", cityDisplay);
@@ -97,29 +96,51 @@ form.addEventListener("submit", apiSearch);
 //api.openweathermap.org/data/2.5/weather?q=canberra&appid=3ec119a7b4622feedeeba843b106eb0a&units=metric`;
 
 //geolocator button temperature
-function buttonWeather(response) {
-  function handlePosition(position) {
-    function weather(response) {
-      let cityHeading = document.querySelector("#cityHeader");
-      let currentLocation = response.data.name;
-      cityHeading.innerHTML = `${currentLocation}`;
 
-      let cityWeather = document.querySelector("#temperature");
-      console.log(response.data);
-      let temperature = Math.round(response.data.main.temp);
-      cityWeather.innerHTML = `${temperature}`;
+function handlePosition(position) {
+  function weather(response) {
+    let cityHeading = document.querySelector("#cityHeader");
+    cityHeading.innerHTML = response.data.name;
+    let cityWeather = document.querySelector("#temperature");
+    console.log(response.data);
+    let temperature = Math.round(response.data.main.temp);
+    cityWeather.innerHTML = `${temperature} °C`;
+    let cityDescription = document.querySelector("#descriptor");
+    console.log(response.data.weather[0].description);
+    cityDescription.innerHTML = response.data.weather[0].description;
+    let apparentTemp = document.querySelector("#feels");
+    let feelTemp = Math.round(response.data.main.feels_like);
+    apparentTemp.innerHTML = `Apparent temperature: Feels like ${feelTemp}°C`;
+    let humidity = document.querySelector("#humidity");
+    let humidityPercent = response.data.main.humidity;
+    humidity.innerHTML = `Humidity: ${humidityPercent}%`;
+    let windSpeed = document.querySelector("#windspeed");
+    let windSpeedms = Math.round(response.data.wind.speed);
+    windSpeed.innerHTML = `Wind speed ${windSpeedms} m/s`;
+    let icon = response.data.weather[0].icon;
+    let iconDisplay = document.querySelector("#icon--today");
+    iconDisplay.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${icon}@2x.png`
+    );
+    function funits(event) {
+      event.preventDefault();
+      let unitsTemp = document.querySelector("#temperature");
+      console.log(temperature.value);
+      let calculate = Math.round(temperature * (9 / 5) + 32);
+      unitsTemp.innerHTML = `${calculate}°F`;
+      let apparentFaren = Math.round(feelTemp * (9 / 5) + 32);
+      apparentTemp.innerHTML = `Apparent temperature: Feels like ${apparentFaren}°F`;
     }
-
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    let key = "3ec119a7b4622feedeeba843b106eb0a";
-    let urlAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
-    axios.get(urlAPI).then(weather);
   }
 
-  navigator.geolocation.getCurrentPosition(handlePosition);
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let key = "3ec119a7b4622feedeeba843b106eb0a";
+  let urlAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
+  axios.get(urlAPI).then(weather);
 }
-let locateButton = document.querySelector("#currentLocation");
-locateButton.addEventListener("click", buttonWeather);
+
+navigator.geolocation.getCurrentPosition(handlePosition);
